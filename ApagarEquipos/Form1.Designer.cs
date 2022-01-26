4a300dbd9ac882e41974f15559c9e4a739415b2e
Celso Alberto Reyes <CelsoAlberto.Reyes@nidec-motor.com>
2022-01-26 08:33:29 -0600

    Add project files.

---
+++ ApagarEquipos/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ApagarEquipos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnApagarPantalla = new System.Windows.Forms.Button();
+            this.btnApagarPC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnApagarPantalla
+            // 
+            this.BtnApagarPantalla.Location = new System.Drawing.Point(110, 187);
+            this.BtnApagarPantalla.Name = "BtnApagarPantalla";
+            this.BtnApagarPantalla.Size = new System.Drawing.Size(105, 38);
+            this.BtnApagarPantalla.TabIndex = 0;
+            this.BtnApagarPantalla.Text = "Apagar Monitor";
+            this.BtnApagarPantalla.UseVisualStyleBackColor = true;
+            this.BtnApagarPantalla.Click += new System.EventHandler(this.BtnApagarPantalla_Click);
+            // 
+            // btnApagarPC
+            // 
+            this.btnApagarPC.Location = new System.Drawing.Point(290, 187);
+            this.btnApagarPC.Name = "btnApagarPC";
+            this.btnApagarPC.Size = new System.Drawing.Size(93, 38);
+            this.btnApagarPC.TabIndex = 1;
+            this.btnApagarPC.Text = "Apagar PC";
+            this.btnApagarPC.UseVisualStyleBackColor = true;
+            this.btnApagarPC.Click += new System.EventHandler(this.btnApagarPC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnApagarPC);
+            this.Controls.Add(this.BtnApagarPantalla);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Apagar Equipo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnApagarPantalla;
+        private System.Windows.Forms.Button btnApagarPC;
+    }
+}
+
---
+++ ApagarEquipos/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Management;
+
+namespace ApagarEquipos
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_SYSCOMMAND = 0x112; //en VB &H112&
+        private const int SC_MONITORPOWER = 0xF170; //en BV &HF170&
+
+        [DllImport("user32.dll")]
+        private extern static void SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void ApagarPC()
+        {
+            Process.Start("shutdown.exe", "-s -f");
+            // s se usará para cerrar un ordenador
+            // f si utilizamos esta extensión todos los programas activos se cerrarán automáticamente.
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ApagarMonitor()
+        {
+            SendMessage(this.Handle, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+            //El 2 es para apagar
+        }
+
+        private void BtnApagarPantalla_Click(object sender, EventArgs e)
+        {
+            ApagarMonitor();
+        }
+
+        private void btnApagarPC_Click(object sender, EventArgs e)
+        {
+            ApagarPC(); 
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Al iniciar el form ejecuta el metodo, revisar para hacerlo a cierta hora.
+            //ApagarMonitor();
+            //ApagarPC();
+        }
+    }
+}
