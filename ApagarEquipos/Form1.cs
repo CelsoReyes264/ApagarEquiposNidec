39ab835b6dbdba5706371a5986c9fd66ee107833
Celso Alberto Reyes <CelsoAlberto.Reyes@nidec-motor.com>
2022-02-08 09:09:24 -0600

    Cambios region.

--- ApagarEquipos/Form1.cs
+++ ApagarEquipos/Form1.cs
@@ -13,6 +13,7 @@ using System.Management;
 using System.Threading;
 using System.Globalization;
 using System.Data.SqlClient;
+using Microsoft.Win32;
 
 namespace ApagarEquipos
 {
@@ -22,21 +23,25 @@ namespace ApagarEquipos
         private const int SC_MONITORPOWER = 0xF170; //en BV &HF170&
         private const int MOUSEEVENTF_MOVE = 0x0001;
 
-        //public const string HoraAlmuerzo = "12:55:00";
-        //public const string HoraRetornoA = "2022-01-26 10:11:00.000";
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetSystemTime(ref SYSTEMTIME st);
 
         [DllImport("user32.dll")]
         private extern static void SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
-        //[DllImport("user32.dll")]
-        //static extern void mouse_event(Int32 dwFlags, Int32 dx, Int32 dy, Int32 dwData, UIntPtr dwExtraInfo);
+
         [DllImport("user32.dll")]
         static extern void mouse_event(Int32 dwFlags, Int32 dx, Int32 dy, Int32 dwData, UIntPtr dwExtraInfo);
 
         public static DateTime HoraActual = DateTime.Now;
-        public static DateTime HoraAlmuerzo = new DateTime(2022, 01, 27, 08, 36, 00);
-        public static DateTime EntradaAlmuerzo = new DateTime(2022, 01, 27, 08, 37, 00);
+
+        #region Horarios
+        public static DateTime HoraAlmuerzoConvertida = DateTime.ParseExact("12:30", "H:mm", null, System.Globalization.DateTimeStyles.None);
+        public static DateTime HoraEntradaAlmuerzo = DateTime.ParseExact("12:35", "H:mm", null, System.Globalization.DateTimeStyles.None);
+        public static DateTime HoraComidaConvertida= DateTime.ParseExact("14:20", "H:mm", null, System.Globalization.DateTimeStyles.None);
+        public static DateTime HoraEntradaComida = DateTime.ParseExact("15:00", "H:mm", null, System.Globalization.DateTimeStyles.None);
         public static DateTime Tiempo = new DateTime();
-        
+        #endregion
+
         [StructLayout(LayoutKind.Sequential)]
         public struct SYSTEMTIME
         {
@@ -50,19 +55,10 @@ namespace ApagarEquipos
             public ushort wMilliseconds;
         }
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool SetSystemTime(ref SYSTEMTIME st);
-        //APAGA POR COMPLETO PC
         private void ApagarPC()
         {
             Process.Start("shutdown.exe", "-s -f");
         }
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
 
         private void ApagarMonitor()
         {
@@ -81,59 +77,46 @@ namespace ApagarEquipos
 
         private static void EncenderMonitor()
         {
-            Form frm = new Form();
             mouse_event(MOUSEEVENTF_MOVE, 0, 1, 0, UIntPtr.Zero);
             Thread.Sleep(40);
             mouse_event(MOUSEEVENTF_MOVE, 0, -1, 0, UIntPtr.Zero);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            ConsultaHora();
-            var x = TimeZoneInfo.ConvertTimeToUtc(Tiempo);
-            SYSTEMTIME st = new SYSTEMTIME();
-            st.wYear = (ushort)x.Year;
-            st.wMonth = (ushort)x.Month;
-            st.wDay = (ushort)x.Day;
-            st.wHour = (ushort)x.Hour;
-            st.wMinute = (ushort)x.Minute;
-            st.wSecond = (ushort)x.Second;
-            SetSystemTime(ref st);
-            tmObtenerTiempo.Enabled = true;
-            tmObtenerTiempo.Interval = 100;
-        } 
-
         private void tmObtenerTiempo_Tick(object sender, EventArgs e)
         {
-             ChecarTiempos();
+            ChecarTiempos();
         }
 
         private void ChecarTiempos()
         {
             HoraActual = DateTime.Now;
-            if (HoraActual >= HoraAlmuerzo &&  HoraActual < EntradaAlmuerzo)
+            if (HoraActual >= HoraAlmuerzoConvertida && HoraActual < HoraEntradaAlmuerzo)
             {
-                while (HoraActual < EntradaAlmuerzo)
+                while (HoraActual < HoraEntradaAlmuerzo)
                 {
                     HoraActual = DateTime.Now;
                     ApagarMonitor();
                 }
                 EncenderMonitor();
             }
-            else
+            else if (HoraActual >= HoraComidaConvertida && HoraActual < HoraEntradaComida)
             {
-
+                while (HoraActual < HoraEntradaComida)
+                {
+                    HoraActual = DateTime.Now;
+                    ApagarMonitor();
+                }
+                EncenderMonitor();
             }
         }
 
         public void ConsultaHora()
         {
-            
             SqlConnection conn = new SqlConnection("Data Source=REY2MXNDSPPAP04;Initial Catalog=SIM;Persist Security Info=True;User ID=Developer;Password=Ac45035350");
             conn.Open();
 
             SqlCommand command = new SqlCommand("exec [Ingenieria].[ObtenerTiempo]", conn);
-            // int result = command.ExecuteNonQuery();
+
             using (SqlDataReader reader = command.ExecuteReader())
             {
                 if (reader.Read())
@@ -144,5 +127,48 @@ namespace ApagarEquipos
             conn.Close();
         }
 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var regKeyGeoId = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Control Panel\International\Geo");
+            var geoID = (string)regKeyGeoId.GetValue("Nation");
+            var allRegions = CultureInfo.GetCultures(CultureTypes.SpecificCultures).Select(x => new RegionInfo(x.ToString()));
+            var regionInfo = allRegions.FirstOrDefault(r => r.GeoId == Int32.Parse(geoID));
+
+            if(regionInfo.Name == "es-MX")
+            {
+                ConsultaHora();
+                var x = TimeZoneInfo.ConvertTimeToUtc(Tiempo);
+                SYSTEMTIME st = new SYSTEMTIME();
+                st.wYear = (ushort)x.Year;
+                st.wMonth = (ushort)x.Month;
+                st.wDay = (ushort)x.Day;
+                st.wHour = (ushort)x.Hour;
+                st.wMinute = (ushort)x.Minute;
+                st.wSecond = (ushort)x.Second;
+                SetSystemTime(ref st);
+                tmObtenerTiempo.Enabled = true;
+                tmObtenerTiempo.Interval = 100;
+            }
+            else if(regionInfo.Name == "chr-Cher-US")
+            {
+                ConsultaHora();
+                var x = TimeZoneInfo.ConvertTimeToUtc(Tiempo);
+                SYSTEMTIME st = new SYSTEMTIME();
+                st.wYear = (ushort)x.Year;
+                st.wMonth = (ushort)x.Month;
+                st.wDay = (ushort)x.Day;
+                st.wHour = (ushort)x.Hour;
+                st.wMinute = (ushort)x.Minute;
+                st.wSecond = (ushort)x.Second;
+                SetSystemTime(ref st);
+                tmObtenerTiempo.Enabled = true;
+                tmObtenerTiempo.Interval = 100;
+            }
+        } 
     }
 }
